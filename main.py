5564e8b941cdcedf002369ad6040359ec785fccc
gabriela21-py <morarugabrieladiana2003@gmail.com>
2025-09-16 13:29:42 +0300

    start

---
+++ main.py
@@ -0,0 +1,9 @@
+message_for_user="Enter todo: "
+todo1=input(message_for_user)
+todo2=input(message_for_user)
+todo3=input(message_for_user)
+
+todos=[todo1,todo2,todo3]
+print(todos)
+
+print(type(todo1))
