8755c268599318c72281193525660dd149e12ff2
gabriela21-py <morarugabrieladiana2003@gmail.com>
2025-10-09 18:40:32 +0300

    update1

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
-message_for_user="Enter todo: "
-todo1=input(message_for_user)
-todo2=input(message_for_user)
-todo3=input(message_for_user)
+todos=[]
 
-todos=[todo1,todo2,todo3]
-print(todos)
+while True:
+    user_action = input("Type add, show, edit,complete or exit: ")
+    user_action = user_action.strip()
+    match user_action:
+        case "add":
+            todo=input("Type a todo: ")
+            todos.append(todo)
+        case "show":
+            for index,item in enumerate(todos):
+                print(f"{index+1}.{item}")
+        case "exit":
+            break
+        case "edit" :
+            number=int(input("Enter the number of the todo to edit: "))
+            number=number-1
+            new_todo=input("Enter thr new todo: ")
+            todos[number]=new_todo
+        case 'complete':
+            number = int(input("Enter the number of the todo to complete: "))
+            number = number - 1
+            todos.pop(number)
+        case _:
+            print("You enter an unknown command")
 
-print(type(todo1))
-print(type(todo2))
+print("Bye!")
